2fcf7601648513485c9a3b69be6c33d11d9315e7
Sergii Tishenko <tishenko.sergey@gmail.com>
2024-09-04 16:54:20 +0200

    add http header

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import htmlPlugin from 'vite-plugin-html-config'
 
+const htmlPluginOpt = {
+  metas: [
+    {
+      "http-equiv": "Content-Security-Policy",
+      content: "upgrade-insecure-requests"
+    },
+  ]
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), htmlPlugin(htmlPluginOpt)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
